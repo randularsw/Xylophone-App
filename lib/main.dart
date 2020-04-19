beab3caf8a76bf3081c01c3ab4fa97b14824e990
Randula Wickramasuriya <randularsw@gmail.com>
2020-04-19 17:40:00 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+          ),
+        ),
+      ),
+    );
+  }
+}
